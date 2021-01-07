a460098b8be723958315814fe345e2e5bd8b7ee2
Sora <8521327+SoraKasvgano@users.noreply.github.com>
2021-01-07 16:03:42 +0800

    Update Bulletin.vue

--- front/src/components/Bulletin.vue
+++ front/src/components/Bulletin.vue
@@ -14,9 +14,8 @@
         data: marked(`
 **网站开发**: [mygoare](https://mygoare.com)
 **BFBAN APP下载**: [cabbagelol](https://app.bfban.com/public/www/)
-**JapaneseContributor**: [RiseJack](https://www.youtube.com/channel/UCptnZeaSNaurVVy9UMWzI6w)
-**EnglishContributor**: [___4Tr1](https://live.bilibili.com/591314)
-**DomainCost**: [【NFZ】BoomNextDoor](https://space.bilibili.com/131470523)
+**JapaneseContributor**: [RiseJack](https://www.youtube.com/channel/UCptnZeaSNaurVVy9UMWzI6w)  **EnglishContributor**: [___4Tr1](https://live.bilibili.com/591314)  **DomainCost**: [【NFZ】BoomNextDoor](https://space.bilibili.com/131470523)
+**战地1私服网页管理工具**: [mygoare](http://ec2-18-163-101-35.ap-east-1.compute.amazonaws.com:4000/#/)
         `, {breaks: true, gfm: true, smartLists: true})
       }
     }
